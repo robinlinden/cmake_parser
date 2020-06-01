bfb36227b1e94210524181b7db468a92255147c4
Robin Linden <dev@robinlinden.eu>
2020-06-01 23:10:53 +0200

    Initial proof-of-concept

---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include <tao/pegtl.hpp>
+
+#include <iostream>
+#include <string_view>
+
+using namespace tao::pegtl;
+
+namespace {
+
+struct space : plus<one<' ', '\t'>> {};
+struct newline : one<'\n'> {};
+struct identifier : seq<identifier_first, star<identifier_other>> {};
+
+struct unquoted_argument : plus<ascii::not_one<'(', ')', '#', '\\', '\'', '>'>> {};
+struct argument : unquoted_argument {};
+struct arguments : opt<argument> {};
+
+struct command_invocation :
+        seq<star<space>, identifier, star<space>, one<'('>, arguments, one<')'>> {};
+
+struct file_element : seq<command_invocation> {};
+
+struct file : star<file_element> {};
+
+struct my_state {
+    std::vector<std::string> identifiers{};
+    std::vector<std::string> arguments{};
+};
+
+template<typename Rule>
+struct my_action : nothing<Rule> {};
+
+template<>
+struct my_action<identifier> {
+    template<typename Input>
+    static void apply(
+            const Input &in,
+            my_state *s) {
+        s->identifiers.push_back(in.string());
+    }
+};
+
+template<>
+struct my_action<argument> {
+    template<typename Input>
+    static void apply(
+            const Input &in,
+            my_state *s) {
+        s->arguments.push_back(in.string());
+    }
+};
+
+constexpr std::string_view str{
+        "identifier(test) Another_one() _this_too(Hello) 5not_this()"};
+
+} // namespace
+
+int main(int argc, char **argv) {
+    my_state s;
+
+    if (argc > 1) {
+        argv_input<> in(argv, 1);
+        parse<file, my_action>(in, &s);
+    } else {
+        memory_input<> in(str.begin(), str.end(), "");
+        parse<file, my_action>(in, &s);
+    }
+
+    std::cout << "Identifiers:";
+    for (const auto &i : s.identifiers) {
+        std::cout << ' ' << i;
+    }
+
+    std::cout << '\n' << "Arguments:";
+
+    for (const auto &i : s.arguments) {
+        std::cout << ' ' << i;
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
